3a8bc220c3821b8a8ec123d14b96f1223b61ad79
Aaron Brezel <aaron.brezel@washpost.com>
2023-06-15 06:48:18 -0700

    removing extra comment

--- entrypoint.R
+++ entrypoint.R
@@ -9,7 +9,6 @@ library(vroom)
 # * via `pr_set_docs()`
 # * or through URL query string variables
 pr_test <- pr("plumber.R") %>% 
-  # (function(pr) pr_get(pr, "/arcos/openapi/", pr$getApiSpec))() %>%
   pr_static("/arcos/__docs__/", "./docs") %>%
   pr_run(
     host='0.0.0.0',
